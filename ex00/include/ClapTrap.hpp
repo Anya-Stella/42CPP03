241b8a057ceefe653c9c78dbd749defff6461792
Tsubasa Ishihara <tishihar@c6r3s4.42tokyo.jp>
2025-08-05 22:12:24 +0900

    init

---
+++ ex00/include/ClapTrap.hpp
@@ -0,0 +1,32 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+// include
+#include <iostream>
+
+
+// class
+class ClapTrap
+{
+private:
+	std::string	_name;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
+
+public:
+
+	// orthodox Canonical Form
+	ClapTrap();
+	ClapTrap(const std::string	&name);
+	ClapTrap(const ClapTrap	&other);
+	ClapTrap	&operator=(const ClapTrap	&rhs);
+	~ClapTrap();
+
+	
+	// Behave
+	void	attack(const std::string	&target);
+	void	takeDamage(unsigned);
+};
+
+#endif
